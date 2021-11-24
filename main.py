2ab624251560e7131c4d2d538350fa8fb4d39152
st10-01-16 <artu.200505@gmail.com>
2021-11-24 18:42:01 +0500

    task 3

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit
 import sys
 
+from ui import Ui_Form
+
 
 SCREEN_SIZE = (800, 500)
 
 
-class Example(QWidget):
+class Example(QWidget, Ui_Form):
     def __init__(self):
         super(Example, self).__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.resize(*SCREEN_SIZE)
         self.pushButton.clicked.connect(self.do_draw)
         self.flag = False
@@ -29,7 +31,7 @@ class Example(QWidget):
             self.qp.end()
 
     def draw(self):
-        self.qp.setBrush(QColor(250, 255, 0))
+        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         x, y = randint(0, SCREEN_SIZE[0]), randint(0, SCREEN_SIZE[1])
         r = randint(0, 100)
         self.qp.drawEllipse(x, y, r, r)
